6e38d438a75c8b0948584b385ef1af36b47e5dac
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-03-26 19:31:13 +0300

    Add left menu to activity_main.xml

--- app/src/main/java/com/kigya/logue/MainActivity.kt
+++ app/src/main/java/com/kigya/logue/MainActivity.kt
@@ -4,10 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.kigya.logue.databinding.ActivityMainBinding
+import com.mikepenz.materialdrawer.AccountHeader
+import com.mikepenz.materialdrawer.AccountHeaderBuilder
+import com.mikepenz.materialdrawer.Drawer
+import com.mikepenz.materialdrawer.DrawerBuilder
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem
+import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mDrawer: Drawer
+    private lateinit var mHeader: AccountHeader
+    private lateinit var mToolbar: androidx.appcompat.widget.Toolbar
+
     private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
@@ -15,6 +28,50 @@ class MainActivity : AppCompatActivity() {
                 viewModel.isloading.value
             }
         }
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        initFields();
+        initFunctionality();
+    }
+
+    private fun initFunctionality() {
+        setSupportActionBar(mToolbar)
+        createHeader()
+        createDrawer()
+    }
+
+    private fun createDrawer() {
+        mDrawer = DrawerBuilder()
+            .withActivity(this)
+            .withToolbar(mToolbar)
+            .withActionBarDrawerToggle(true)
+            .withSelectedItem(-1)
+            .withAccountHeader(mHeader)
+            .addDrawerItems(
+                PrimaryDrawerItem()
+                    .withIdentifier(100)
+                    .withIconTintingEnabled(true)
+                    .withName("Create group")
+                    .withSelectable(false)
+            ).build()
+    }
+
+    private fun createHeader() {
+        mHeader = AccountHeaderBuilder()
+            .withActivity(this)
+            .withHeaderBackground(R.drawable.header)
+            .addProfiles(
+                ProfileDrawerItem()
+                    .withName("John Doe")
+                    .withEmail("+71234567")
+            ).build()
+    }
+
+    private fun initFields() {
+        mToolbar = mBinding.mainToolbar
     }
 }
