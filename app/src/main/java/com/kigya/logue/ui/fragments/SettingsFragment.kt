aba8be9a3876acceaa70daf989845e914f04ee8a
kirillborichevskiy <kirilborichevskiy@gmail.com>
2022-03-27 20:32:18 +0300

    Add action bar settings options

--- app/src/main/java/com/kigya/logue/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/kigya/logue/ui/fragments/SettingsFragment.kt
@@ -1,11 +1,17 @@
 package com.kigya.logue.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import com.kigya.logue.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu, menu)
+    }
 }
